c28b77240319e0c56ca2b82f32d06593bf9acd00
Annah <annah.gerletti@fluxfederation.com>
2018-09-07 11:11:53 +1200

    Four Passing tests
    finished specing the New and Create actions from the game controller.

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -13,31 +13,41 @@ RSpec.describe GamesController do
   describe "POST #create" do
     # let (:name) { "Name"}
     # let (:lives) { 5 }
-    # let (:word) { "bird"}
-    let! (:game) {Game.create}
+    let! (:word) { Word.create!(word: "example") }
+    # let! (:game) {Game.create}
     # let (:params) { name: "Chris"}
     
 
-    context "when a game is started with a name" do
-      it "redirects to game path" do
-        params = {:game => {lives: 3, name: 'Ham'}}
-        location = game_path(:game)
+    context "when game is started with a name" do
+      let (:new_game) {Game.create}
 
-        post (:create, params: params)
+      it "redirects to show template on game path" do
+        params = {:game => {lives: 3, name: 'Ham'}}
+        
+        # def post(method, options)
+        post(:create, :params => params)
+        # had to specify the correct game_id to get to the game path (show)
+        location = game_path(Game.last)
 
         expect(response).to redirect_to(location)
         
       end
 
       it 'saves to the database' do
+        params = {:game => {lives: 3, name: 'Ham'}}
+
+
+        expect{post(:create, :params => params)}.to change(Game, :count).by(1)
       end
     end
     
+    
     context "when the game is started without a name" do
-      it "renders the new template" do 
+      it "renders the new template" do
         params = {:game => {lives: 2}}
 
-        post (:create, params: params)
+        # def post(method, params:)
+        post(:create, params: params)
 
         expect(response).to render_template(:new)
       end
