485e5c0b197ac9e8d02f8e704af06fd94f84b267
Annah <annah.gerletti@fluxfederation.com>
2018-09-21 15:51:55 +1200

    The extra stuff
    Changes made to my fixtures, removed some code from the validations for guess.

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -1,9 +1,14 @@
+# frozen_string_literal: true
+
 class Guess < ApplicationRecord
   belongs_to :game
-    validates :guess, presence: true
-    validates :guess, length: { is: 1}
-    validates :guess, format: { with: /\A[a-zA-Z]+\z/, 
-    message: "only allows letters"}
-    # have to be alpha characters, A - z - says  alphnumeric character has to be at the start and end of string.
-    #find some validation that prevents Numbers being entered
+  validates :guess, presence: true
+  validates :guess, length: { is: 1 }
+  validates :guess, format: { with: /\A[a-zA-Z]+\z/ }
+  # have to be alpha characters, A - z - says  alphnumeric character has to be at the start and end of string.
+  # find some validation that prevents Numbers being entered
 end
+
+# When you create a guess it has an accosiated game bc it
+# guess.new(game_id => 1)
+# test all thew validation situations
